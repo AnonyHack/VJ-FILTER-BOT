438f6e5c4b8cb351c069aa26b0428c59766f3219
Ramy Phoenix <recoveryfreenethub@gmail.com>
2025-04-10 15:11:00 +0200

    Update Script.py

--- Script.py
+++ Script.py
@@ -5,7 +5,7 @@
 class script(object):
     START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {} 👋,</blockquote>
     
-ɪ ᴀᴍ ʟᴀᴛᴇꜱᴛ ᴀᴅᴠᴀɴᴄᴇᴅ ᴀɴᴅ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏ ꜰɪʟᴛᴇʀ ʙᴏᴛ. ʏᴏᴜ ᴄᴀɴ ᴜꜱᴇ ᴍᴇ ɪɴ ʏᴏᴜʀ ɢʀᴏᴜᴘ ᴛᴏ ᴇᴀʀɴ ᴜɴʟɪᴍɪᴛᴇᴅ ᴍᴏɴᴇʏ...💸</b>"""
+I Aᴍ Lᴀᴛᴇꜱᴛ Aᴅᴠᴀɴᴄᴇᴅ Mᴏᴠɪᴇ Fɪʟᴛᴇʀ/Sᴇᴀʀᴄʜ Bᴏᴛ. Yᴏᴜ Cᴀɴ Aʟꜱᴏ Uꜱᴇ Mᴇ Tᴏ Eᴀʀɴ Mᴏɴᴇʏ Tʜʀᴏᴜɢʜ Lɪɴᴋ Sʜᴏʀᴛᴇɴᴇʀ Wᴇʙꜱɪᴛᴇꜱ...💸</b>"""
 
     CLONE_START_TXT = """<b><blockquote>ʜᴇʟʟᴏ {}, ᴍʏ ɴᴀᴍᴇ <a href=https://t.me/{}>{}</a></blockquote>
     
